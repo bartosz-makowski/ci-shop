c6ae85d66e7c5fbdcfab43c0d2b5725b40587d1c
Bartek_makowski <mako19895@gmail.com>
2021-03-11 01:15:44 +0000

    tutorial 1 from toasts completed

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
 from products.models import Product
 
 
@@ -10,6 +11,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
+
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
