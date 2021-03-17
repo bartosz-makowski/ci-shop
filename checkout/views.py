05966c2efaccfc3d16b150367d5f6c659d5e804c
Bartek_makowski <mako19895@gmail.com>
2021-03-17 00:54:32 +0000

    tutorial 2 stripe finished

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IVmkoB40bdXcAKn8SBlenDSXvLozCk2GM8Fyn3fbMOcvIhOROH0Vtp5PbKz8dDfAAIVCgQDH6fGGmMwTFd02Cdv000KQcbstJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
